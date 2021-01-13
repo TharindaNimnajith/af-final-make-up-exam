5b2884699bb7e33323b43eb0162eec41449d92a1
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-13 14:56:15 +0530

    test

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
 const HttpError = require('./models/http-errors')
 const RoomsRoutes = require('./routes/rooms.routes')
 
