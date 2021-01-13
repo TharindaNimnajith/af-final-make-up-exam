93b80b6b0825ac56ac3463fbe671e5459a2c06f0
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-14 01:55:44 +0530

    frontend

---
+++ frontend/src/context/app-context.js
@@ -0,0 +1,7 @@
+import {createContext} from 'react';
+
+export const AppContext = createContext({
+  accessToken: null,
+  login: () => {
+  }
+});
---
+++ frontend/src/context/global-state.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import {AppContext} from './app-context';
+
+export const GlobalState = props => {
+  const [accessToken, setAccessToken] = useState('');
+
+  const login = accessToken => {
+    setAccessToken(accessToken);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        accessToken: accessToken,
+        login: login
+      }}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
