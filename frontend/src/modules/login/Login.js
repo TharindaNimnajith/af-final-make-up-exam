d05d30b3fdfec2ede374e3e70d599365f6a2d2e7
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-01-14 12:42:19 +0530

    frontend

--- frontend/src/modules/login/Login.js
+++ frontend/src/modules/login/Login.js
@@ -3,14 +3,13 @@ import {Card, CardBody} from 'reactstrap'
 import {Link} from 'react-router-dom'
 import get from 'lodash.get'
 import axios from 'axios'
-
-import './login.css'
+import {authStoreKey} from '../../configs/config'
+import {isEmpty} from '../../helpers/common.helpers'
+import {setLocalStorageItem} from '../../helpers/local-storage.helpers'
 import TextField from '../../components/text-field/TextField'
 import ButtonComponent from '../../components/button/Button'
-import {isEmpty, setLocalStorageItem} from '../../helpers/common.helpers'
 import Loader from '../../components/loader/Loader'
-import {authAPI, authStoreKey} from '../../configs/config'
-
+import './login.css'
 
 class Login extends Component {
   constructor(props) {
@@ -34,33 +33,29 @@ class Login extends Component {
   }
 
   onClickFn = () => {
-    let {history} = this.props
+    let {
+      history
+    } = this.props
 
     if (this.validateFn(this.state.formData)) {
-
       this.setState({
         ...this.state,
         loader: true,
         inputErrors: {}
       }, () => {
-
-        axios.post(authAPI, this.state.formData)
-          .then((result) => {
-
-            setLocalStorageItem(authStoreKey, result.data.data)
-            history.push('/')
+        axios.post(authAPI, this.state.formData).then((result) => {
+          setLocalStorageItem(authStoreKey, result.data.data)
+          history.push('/')
+        }).catch((err) => {
+          this.setState({
+            ...this.state,
+            loader: false,
+            inputErrors: {
+              email: 'Email or Password is incorrect',
+              password: 'Email or Password is incorrect',
+            }
           })
-          .catch((err) => {
-
-            this.setState({
-              ...this.state,
-              loader: false,
-              inputErrors: {
-                email: 'Email or Password is incorrect',
-                password: 'Email or Password is incorrect',
-              }
-            })
-          })
+        })
       })
     }
   }
@@ -68,20 +63,17 @@ class Login extends Component {
   validateFn = (formObj) => {
     let errors = {}
 
-    if (isEmpty(formObj.email)) {
+    if (isEmpty(formObj.email))
       errors['email'] = 'Email is required'
-    }
 
-    if (isEmpty(formObj.password)) {
+    if (isEmpty(formObj.password))
       errors['password'] = 'Password is required'
-    }
 
     if (Object.keys(errors).length > 0) {
       this.setState({
         ...this.state,
         inputErrors: errors
       })
-
       return false
     } else {
       return true
@@ -97,63 +89,49 @@ class Login extends Component {
           ) : null
         }
         <div className={'loginWrapper'}>
-
           <Card className={'overflow-hidden'}>
             <div className={'loginHeader'}>
               <div className={'text-primary text-center p-4'}>
-
                 <h5 className={'text-white font-size-20'}>
-                  Welcome Back !
+                  Welcome Back!
                 </h5>
-
                 <p className={'text-white-50'}>
                   Sign into Online Book Store
                 </p>
-
                 <img src={'https://www.strunkmedia.com/wp-content/uploads/2018/05/bigstock-Print-163213010.png'}
-                     height='50' alt='logo'/>
-
+                     height='50'
+                     alt='logo'/>
               </div>
             </div>
-
             <CardBody className={'p-4'}>
               <div className={'p-3'}>
-                <TextField
-                  isRequired={true}
-                  labelText={'Email'}
-                  name={'email'}
-                  value={get(this.state, 'formData.email', '')}
-                  errorText={get(this.state, 'inputErrors.email', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
-                <TextField
-                  isRequired={true}
-                  labelText={'Password'}
-                  type={'password'}
-                  name={'password'}
-                  value={get(this.state, 'formData.password', '')}
-                  errorText={get(this.state, 'inputErrors.password', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
+                <TextField isRequired={true}
+                           labelText={'Email'}
+                           name={'email'}
+                           value={get(this.state, 'formData.email', '')}
+                           errorText={get(this.state, 'inputErrors.email', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
+                <TextField isRequired={true}
+                           labelText={'Password'}
+                           type={'password'}
+                           name={'password'}
+                           value={get(this.state, 'formData.password', '')}
+                           errorText={get(this.state, 'inputErrors.password', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
                 <div className={'d-flex justify-content-end'}>
-                  <ButtonComponent
-                    btnText={'Login'}
-                    isFullWidth={false}
-                    elementStyle={'loginBtn'}
-                    onClickFn={this.onClickFn}
-                  />
+                  <ButtonComponent btnText={'Login'}
+                                   isFullWidth={false}
+                                   elementStyle={'loginBtn'}
+                                   onClickFn={this.onClickFn}/>
                 </div>
               </div>
-
               <div className={'text-center'}>
-                <Link to={'/register'}>Click here to register</Link>
+                <Link to={'/register'}>
+                  Click here to register
+                </Link>
               </div>
             </CardBody>
-
           </Card>
-
         </div>
       </div>
     )
--- frontend/src/modules/register/Register.js
+++ frontend/src/modules/register/Register.js
@@ -3,14 +3,11 @@ import {Card, CardBody} from 'reactstrap'
 import get from 'lodash.get'
 import axios from 'axios'
 import Swal from 'sweetalert2'
-
-import '../login/login.css'
+import {isEmpty} from '../../helpers/common.helpers'
 import TextField from '../../components/text-field/TextField'
 import ButtonComponent from '../../components/button/Button'
-import {isEmpty} from '../../helpers/common.helpers'
 import Loader from '../../components/loader/Loader'
-import {userAPI} from '../../configs/config'
-
+import '../login/login.css'
 
 class Register extends Component {
   constructor(props) {
@@ -34,41 +31,36 @@ class Register extends Component {
   }
 
   onSubmitFn = () => {
-    let {history} = this.props
+    let {
+      history
+    } = this.props
 
     if (this.validateFn(this.state.formData)) {
-
       this.setState({
         ...this.state,
         inputErrors: {},
         loader: true
       }, () => {
-
-        axios.post(userAPI, this.state.formData)
-          .then((result) => {
-
-            this.setState({
-              ...this.state,
-              loader: false
-            }, () => {
-
-              Swal.fire({
-                icon: 'success',
-                title: 'Successfully Registered',
-                showConfirmButton: false,
-                timer: 2000
-              })
-                .then((result) => {
-                  history.push('/')
-                })
+        axios.post(userAPI, this.state.formData).then((result) => {
+          this.setState({
+            ...this.state,
+            loader: false
+          }, () => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Successfully Registered',
+              showConfirmButton: false,
+              timer: 2000
+            }).then((result) => {
+              history.push('/')
             })
           })
-          .catch((error) => {
-            this.setState({
-              ...this.state,
-              loader: false
-            })
+        }).catch((error) => {
+          this.setState({
+            ...this.state,
+            loader: false
           })
+        })
       })
     }
   }
@@ -76,32 +68,25 @@ class Register extends Component {
   validateFn = (formObj) => {
     let errors = {}
 
-    if (isEmpty(formObj.full_name)) {
+    if (isEmpty(formObj.full_name))
       errors['full_name'] = 'Full Name is required'
-    }
 
-    if (isEmpty(formObj.email)) {
+    if (isEmpty(formObj.email))
       errors['email'] = 'Email is required'
-    }
 
-    if (isEmpty(formObj.password)) {
+    if (isEmpty(formObj.password))
       errors['password'] = 'Password is required'
-    }
 
-    if (isEmpty(formObj.confirm_password)) {
+    if (isEmpty(formObj.confirm_password))
       errors['confirm_password'] = 'Confirm Password is required'
-    } else {
-      if (formObj.password !== formObj.confirm_password) {
-        errors['confirm_password'] = 'Password and Confirm Password should be same'
-      }
-    }
+    else if (formObj.password !== formObj.confirm_password)
+      errors['confirm_password'] = 'Password and Confirm Password should be same'
 
     if (Object.keys(errors).length > 0) {
       this.setState({
         ...this.state,
         inputErrors: errors
       })
-
       return false
     } else {
       return true
@@ -117,78 +102,57 @@ class Register extends Component {
           ) : null
         }
         <div className={'loginWrapper'}>
-
           <Card className={'overflow-hidden'}>
             <div className={'loginHeader'}>
               <div className={'text-primary text-center p-4'}>
-
                 <h5 className={'text-white font-size-20'}>
                   Register
                 </h5>
-
                 <p className={'text-white-50'}>
                   Register into Online Book Store
                 </p>
-
                 <img src={'https://www.strunkmedia.com/wp-content/uploads/2018/05/bigstock-Print-163213010.png'}
-                     height='50' alt='logo'/>
-
+                     height='50'
+                     alt='logo'/>
               </div>
             </div>
-
             <CardBody>
               <div>
-                <TextField
-                  isRequired={true}
-                  labelText={'Full Name'}
-                  name={'full_name'}
-                  value={get(this.state, 'formData.full_name', '')}
-                  errorText={get(this.state, 'inputErrors.full_name', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
-                <TextField
-                  isRequired={true}
-                  labelText={'Email'}
-                  name={'email'}
-                  value={get(this.state, 'formData.email', '')}
-                  errorText={get(this.state, 'inputErrors.email', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
-                <TextField
-                  isRequired={true}
-                  labelText={'Password'}
-                  type={'password'}
-                  name={'password'}
-                  value={get(this.state, 'formData.password', '')}
-                  errorText={get(this.state, 'inputErrors.password', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
-                <TextField
-                  isRequired={true}
-                  labelText={'Confirm Password'}
-                  type={'password'}
-                  name={'confirm_password'}
-                  value={get(this.state, 'formData.confirm_password', '')}
-                  errorText={get(this.state, 'inputErrors.confirm_password', '')}
-                  onChangeFn={(event) => this.onChangeFn(event)}
-                />
-
+                <TextField isRequired={true}
+                           labelText={'Full Name'}
+                           name={'full_name'}
+                           value={get(this.state, 'formData.full_name', '')}
+                           errorText={get(this.state, 'inputErrors.full_name', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
+                <TextField isRequired={true}
+                           labelText={'Email'}
+                           name={'email'}
+                           value={get(this.state, 'formData.email', '')}
+                           errorText={get(this.state, 'inputErrors.email', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
+                <TextField isRequired={true}
+                           labelText={'Password'}
+                           type={'password'}
+                           name={'password'}
+                           value={get(this.state, 'formData.password', '')}
+                           errorText={get(this.state, 'inputErrors.password', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
+                <TextField isRequired={true}
+                           labelText={'Confirm Password'}
+                           type={'password'}
+                           name={'confirm_password'}
+                           value={get(this.state, 'formData.confirm_password', '')}
+                           errorText={get(this.state, 'inputErrors.confirm_password', '')}
+                           onChangeFn={(event) => this.onChangeFn(event)}/>
                 <div className={'d-flex justify-content-end'}>
-                  <ButtonComponent
-                    btnText={'Register'}
-                    isFullWidth={false}
-                    elementStyle={'loginBtn'}
-                    onClickFn={this.onSubmitFn}
-                  />
+                  <ButtonComponent btnText={'Register'}
+                                   isFullWidth={false}
+                                   elementStyle={'loginBtn'}
+                                   onClickFn={this.onSubmitFn}/>
                 </div>
               </div>
             </CardBody>
-
           </Card>
-
         </div>
       </div>
     )
